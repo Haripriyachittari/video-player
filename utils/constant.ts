2672eb5996c0d4b9cba966ee768cef0e5013ae21
Haripriya Chittari <hpvarma09@gmail.com>
2024-03-04 20:56:18 +0530

    Update constant.ts

--- utils/constant.ts
+++ utils/constant.ts
@@ -79,17 +79,6 @@ const mediaJSON = {
             "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
           title: "For Bigger Meltdowns",
         },
-        // {
-        //   description:
-        //     "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
-        //   sources: [
-        //     "https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg4",
-        //   ],
-        //   subtitle: "By Blender Foundation",
-        //   thumb:
-        //     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg",
-        //   title: "Sintel",
-        // },
         {
           description:
             "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
@@ -112,39 +101,7 @@ const mediaJSON = {
             "https://storage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg",
           title: "Tears of Steel",
         },
-        // {
-        //   description:
-        //     "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
-        //   sources: [
-        //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
-        //   ],
-        //   subtitle: "By Garage419",
-        //   thumb:
-        //     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/VolkswagenGTIReview.jpg",
-        //   title: "Volkswagen GTI Review",
-        // },
-        // {
-        //   description:
-        //     "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
-        //   sources: [
-        //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
-        //   ],
-        //   subtitle: "By Garage419",
-        //   thumb:
-        //     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/WeAreGoingOnBullrun.jpg",
-        //   title: "We Are Going On Bullrun",
-        // },
-        // {
-        //   description:
-        //     "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
-        //   sources: [
-        //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4",
-        //   ],
-        //   subtitle: "By Garage419",
-        //   thumb:
-        //     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/WhatCarCanYouGetForAGrand.jpg",
-        //   title: "What care can you get for a grand?",
-        // },
+      
       ],
     },
   ],
